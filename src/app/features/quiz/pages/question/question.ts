b9bb08eea766a5078ff88c1e7dcdc9267e08cae6
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-19 19:51:51 -0600

    feat: add question page component

---
+++ src/app/features/quiz/pages/question/question.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-question',
+  styleUrl: './question.scss',
+  templateUrl: './question.html',
+})
+export class Question {}
