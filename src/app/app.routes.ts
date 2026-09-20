cbf741138d2497d7cf44191af394a184aa99cb73
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-19 19:56:03 -0600

    feat: configure quiz application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/quiz/pages/home/home').then(
+        (component) => component.Home
+      ),
+  },
+  {
+    path: 'instrucciones',
+    loadComponent: () =>
+      import('./features/quiz/pages/instructions/instructions').then(
+        (component) => component.Instructions
+      ),
+  },
+  {
+    path: 'mapa',
+    loadComponent: () =>
+      import('./features/quiz/pages/map/map').then(
+        (component) => component.Map
+      ),
+  },
+  {
+    path: 'quiz',
+    loadComponent: () =>
+      import('./features/quiz/pages/question/question').then(
+        (component) => component.Question
+      ),
+  },
+  {
+    path: 'resultado',
+    loadComponent: () =>
+      import('./features/quiz/pages/result/result').then(
+        (component) => component.Result
+      ),
+  },
+  {
+    path: 'insignias',
+    loadComponent: () =>
+      import('./features/quiz/pages/badges/badges').then(
+        (component) => component.Badges
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
