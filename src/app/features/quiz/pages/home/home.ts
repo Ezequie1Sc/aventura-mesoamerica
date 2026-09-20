fe5e2c2e4e5171f311865d5b18235d8800c9efa2
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-19 20:06:34 -0600

    feat: add home page component

--- src/app/features/quiz/pages/home/home.ts
+++ src/app/features/quiz/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-home',
-  styleUrl: './home.scss',
+  imports: [RouterLink],
   templateUrl: './home.html',
+  styleUrl: './home.scss',
 })
 export class Home {}
