519f73a99ff85f05890a2cbd06d09489dbde112c
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-19 21:04:52 -0600

    feat: add home help dialog

--- src/app/features/quiz/pages/home/home.ts
+++ src/app/features/quiz/pages/home/home.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  @ViewChild('helpDialog')
+  private helpDialog!: ElementRef<HTMLDialogElement>;
+
+  openHelp(): void {
+    const dialog = this.helpDialog.nativeElement;
+
+    if (!dialog.open) {
+      dialog.showModal();
+    }
+  }
+
+  closeOnBackdrop(event: MouseEvent): void {
+    const dialog = this.helpDialog.nativeElement;
+
+    if (event.target !== dialog) {
+      return;
+    }
+
+    const rect = dialog.getBoundingClientRect();
+
+    const clickedOutside =
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom;
+
+    if (clickedOutside) {
+      dialog.close();
+    }
+  }
+}
