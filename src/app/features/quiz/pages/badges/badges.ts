b2a29d1977b3696d46a2e59e5dea6307f5501185
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-19 19:51:44 -0600

    feat: add badges page component

---
+++ src/app/features/quiz/pages/badges/badges.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-badges',
+  styleUrl: './badges.scss',
+  templateUrl: './badges.html',
+})
+export class Badges {}
